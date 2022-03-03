77a211693c60d419e036697899e528a3937adcdb
Terry <tmalden69@rogers.com>
2022-03-03 12:02:59 -0500

    Initial Commit

---
+++ War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Terry Malden on 2022-03-03.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
